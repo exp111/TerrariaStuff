145624550f11a4daf49c6ee69bea0e365f883767
Exp <theexp111@gmail.com>
2019-08-13 17:52:44 +0200

    Removed recipe

--- ChallengeMod/Items/ChallengeOrb.cs
+++ ChallengeMod/Items/ChallengeOrb.cs
@@ -22,15 +22,6 @@ namespace ChallengeMod.Items
 			item.rare = 11;
 		}
 
-		public override void AddRecipes() 
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-
 		public override bool UseItem(Player player)
 		{
 			ChallengeMod.Instance.ToggleUIVisible();
